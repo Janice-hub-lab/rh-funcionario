556a6e95ad50514af36ecefa7e7b1925870d697f
Jane Gomes <asscontecjane@gmail.com>
2024-09-25 13:01:04 -0700

    inicia o projeto

---
+++ src/test/java/br/com/rh/rh_funcionario/RhFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rh.rh_funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
