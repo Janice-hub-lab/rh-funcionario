031f03374df11b8d662dc503f97446eb2dce7b58
Jane Gomes <asscontecjane@gmail.com>
2024-09-27 16:30:01 -0700

    implementa request e response

--- src/main/java/br/com/rh/funcionario/empresa_funcionario/empresa/application/api/EmpresaRequest.java
+++ src/main/java/br/com/rh/funcionario/empresa_funcionario/empresa/application/api/EmpresaRequest.java
@@ -1,5 +1,27 @@
 package br.com.rh.funcionario.empresa_funcionario.empresa.application.api;
 
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class EmpresaRequest {
+	@NotBlank
+	private String nomeCompletoEmpresa;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	@NotNull
+	private LocalDate dataInicio;
+	@CNPJ
+	private String cnpj;
 
 }
--- src/main/java/br/com/rh/funcionario/empresa_funcionario/empresa/application/api/EmpresaResponse.java
+++ src/main/java/br/com/rh/funcionario/empresa_funcionario/empresa/application/api/EmpresaResponse.java
@@ -1,5 +1,11 @@
 package br.com.rh.funcionario.empresa_funcionario.empresa.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class EmpresaResponse {
+	private UUID idEmpresa;
 
 }
