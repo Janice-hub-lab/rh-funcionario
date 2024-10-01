399e77e42534c5442db872d35d9588a12c0ce165
Jane Gomes <asscontecjane@gmail.com>
2024-10-01 16:01:37 -0700

    implementa request e response

--- src/main/java/br/com/rh/funcionario/empresa_funcionario/funcionario/application/api/FuncionarioRequest.java
+++ src/main/java/br/com/rh/funcionario/empresa_funcionario/funcionario/application/api/FuncionarioRequest.java
@@ -1,5 +1,21 @@
 package br.com.rh.funcionario.empresa_funcionario.funcionario.application.api;
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class FuncionarioRequest {
+	@NotBlank
+	private String nomeCompletoFuncionario;
+	@NotBlank
+	private String cargo;
+	private double salario;
+	private String telefone;
+	private String endereco;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
 
 }
--- src/main/java/br/com/rh/funcionario/empresa_funcionario/funcionario/application/api/FuncionarioResponse.java
+++ src/main/java/br/com/rh/funcionario/empresa_funcionario/funcionario/application/api/FuncionarioResponse.java
@@ -1,5 +1,11 @@
 package br.com.rh.funcionario.empresa_funcionario.funcionario.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class FuncionarioResponse {
+	private UUID idFuncionario;
 
 }
